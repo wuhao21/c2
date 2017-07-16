b9df9a4297392477e28ed546dfff2ee09ac7e7dc
WuHao <wu.hao.cz.21@gmail.com>
2017-07-16 08:28:34 -0700

    add readme

--- group/urls.py
+++ group/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^(?P<gid>[0-9]+\-[1-3])/$', views.detail, name='group-detail'),
     url(r'^g/(?P<gid>[0-9]+\-[1-3])/$', views.generate, name='group-generate'),
+    url(r'^r/(?P<gid>[0-9]+\-[1-3])/$', views.readme, name='group-readme'),
 ]
--- group/views.py
+++ group/views.py
@@ -7,8 +7,24 @@ from group.models import Group
 import os
 f = open('indexmd.txt')
 content = f.read()
+f.close()
 
 # Create your views here.
+
+def readme(request, gid):
+    request.encoding='utf-8'
+    try:
+        username = request.session['who']
+    except KeyError:
+        return render(request, 'index.html' , {'content': content, 'status': 'old', 'username':''})
+    try:
+        group = Group.objects.get(gid=gid)
+    except Group.DoesNotExist:
+        raise Http404
+    f = open("data/%s/README"%gid)
+    content = f.read()
+    f.close()
+    return HttpResponse(content)
 
 def generate(request, gid):
     request.encoding='utf-8'
@@ -34,4 +50,4 @@ def detail(request, gid):
         group = Group.objects.get(gid=gid)
     except Group.DoesNotExist:
         raise Http404
-    return render(request, 'group/detail.html', {'group' : group, 'username' : username})
+    return render(request, 'group/detail.html', {'group' : group, 'username' : username, 'readme' : ""})
